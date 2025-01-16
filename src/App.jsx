52907f12f59075c13284037e032a4b3ed5e462d9
Sunil khadka <sunil@vrittechnologies.com>
2025-01-16 12:18:19 +0545

    navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { Navigate, useNavigate } from "react-router";
 import "./App.css";
 import TodoApp from "./TODO/TodoApp";
 
 function App() {
+	const navigate = useNavigate();
 	return (
 		<div className="main-container">
 			<h1 className="title-bar">APP LISTS</h1>
 			<div className="apps">
 				<button className="todo-app">TODO APP</button>
-				<button className="modal-app">MODAL DEMO</button>
+				<button className="modal-app" onClick={() => navigate("/about")}>
+					MODAL DEMO
+				</button>
 				{/* <TodoApp /> */}
 			</div>
 		</div>
